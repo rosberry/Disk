41b64c1af6721c1e89b48b16b0c49f3556bd0d33
Anton Kormakov <anton.kormakov@rosberry.com>
2021-09-02 12:43:52 +0600

    Set spm sources path

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
+    path: "Sources",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
